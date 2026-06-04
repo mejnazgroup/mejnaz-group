1b9226195bbc1b69a9b0a18c8fa6330d4d71c233
MEJNAZ Group <mejnazgroup@gmail.com>
2026-06-04 14:56:14 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from '@/components/mejnaz/Hero';
 import Stats from '@/components/mejnaz/Stats';
 import Services from '@/components/mejnaz/Services';
 import WhyUs from '@/components/mejnaz/WhyUs';
-import Testimonials from '@/components/mejnaz/Testimonials';
 import Contact from '@/components/mejnaz/Contact';
 import Footer from '@/components/mejnaz/Footer';
 import FloatingWhatsApp from '@/components/mejnaz/FloatingWhatsApp';
@@ -16,7 +15,6 @@ export default function Home() {
       <Stats />
       <Services />
       <WhyUs />
-      <Testimonials />
       <Contact />
       <Footer />
       <FloatingWhatsApp />
